03784d2668ad234dd61f72c94fcbf9bccb5052d0
jlounsb1 <jefflounsbury@gmail.com>
2024-08-09 14:59:06 -0400

    added generic error handling

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded())
 
+await mongoose.connect(process.env.ATLAS_URI)
+//connect to mongoose
+
 //test to make sure server running
 app.get('/', async (req, res)=> {
     let userList = await User.find({})
     res.send(userList)
 })
 
-await mongoose.connect(process.env.ATLAS_URI)
-//connect to mongoose
 
+//Generic error handling
+app.use((err, req, res, next) => {
+    console.error(err)
+    res.status(500).send("Ok now it seems we are lost, I'm afraid your pupper will have to play alone.", {error:err});
+  });
+  
+//listen setup
 app.listen(PORT, ()=>{
     console.log(`server is listening at port ${PORT}`)
 })
