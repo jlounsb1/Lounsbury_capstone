1c838102088d17b85c685d493b2e024fc8dffc6f
jlounsb1 <jefflounsbury@gmail.com>
2024-08-09 14:41:52 -0400

    added a few items to schema for users

--- backend/models/users.mjs
+++ backend/models/users.mjs
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-    username:{type:String}
+    username:{type:String},
+    password:{type:String},
+    email:{type:String},
+    
 })
 
 export default mongoose.model('User', userSchema)
