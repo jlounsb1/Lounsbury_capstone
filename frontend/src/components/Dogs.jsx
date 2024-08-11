b6731af870615898d432932efced98c4575ae428
jlounsb1 <jefflounsbury@gmail.com>
2024-08-11 14:39:04 -0400

    added data pull to try and map dogs

--- frontend/src/components/Dogs.jsx
+++ frontend/src/components/Dogs.jsx
@@ -1,9 +1,29 @@
+
+import OtherDog from './OtherDog'
 import React from 'react'
+import {useState} from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 export default function Dogs() {
+    axios.get(`http://localhost:3000/dogs`)
+    .then(result =>console.log(result))
+    .catch(err =>console.log(err))
+   //I can get the data to console, but I can't get it to return, and therefor be able to be mapped
     return (
         <>
-        <h3>This is where the list of dogs, and your dogs information will show.</h3>
+       <div className="matchingChart">
+       <div className="yourInfo">
+
+       </div>
+       <div className="otherDog">
+            <OtherDog >
+             
+            </OtherDog>
+
+       </div>
+        </div>
         </>
     )
 }
--- frontend/src/components/PlayDates.jsx
+++ frontend/src/components/PlayDates.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import {useState} from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
 
 export default function PlayDates() {
+
+
     return(
-        <>
-        <h3>
-            This is where the list of playdates will be
-        </h3>
-        </>
+       <>
+       <h3>List of playdates for your dog</h3>
+      
+       </>
     )
 }
