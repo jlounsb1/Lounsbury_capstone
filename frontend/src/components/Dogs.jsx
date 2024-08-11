f01e81afc90d89d24d4ebd31864fd85506e53f79
jlounsb1 <jefflounsbury@gmail.com>
2024-08-11 15:27:03 -0400

    trying to get the request into a variable

--- frontend/src/components/Dogs.jsx
+++ frontend/src/components/Dogs.jsx
@@ -7,21 +7,17 @@ import {Link} from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 
 export default function Dogs() {
-    axios.get(`http://localhost:3000/dogs`)
-    .then(result =>console.log(result))
-    .catch(err =>console.log(err))
+
    //I can get the data to console, but I can't get it to return, and therefor be able to be mapped
     return (
         <>
        <div className="matchingChart">
        <div className="yourInfo">
-
+        <h3>Your dogs info</h3>
        </div>
        <div className="otherDog">
-            <OtherDog >
-             
-            </OtherDog>
-
+        <h3>Other Dogs To play with:</h3>
+            <OtherDog />
        </div>
         </div>
         </>
--- frontend/src/components/OtherDog.jsx
+++ frontend/src/components/OtherDog.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import axios from 'axios'
 
 export default function OtherDog(){
-    
+
+axios.get(`http://localhost:3000/dogs`)
+.then((result) =>{
+    console.log(result);
+ })
+.catch(err =>console.log(err))
+
 return(
     <>
     
