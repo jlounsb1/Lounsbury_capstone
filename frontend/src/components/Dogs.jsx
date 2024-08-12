ac0994d2e55af539cb5c5f3c4debdb6f29b746f4
jlounsb1 <jefflounsbury@gmail.com>
2024-08-12 18:29:15 -0400

    added a request for all dogs, and mapped it to the doglist page. added link to create a date

--- frontend/src/components/Dogs.jsx
+++ frontend/src/components/Dogs.jsx
@@ -14,6 +14,7 @@ import{useParams} from 'react-router-dom'
 
 export default function Dogs(props) {
    const [yourDog, setYourDog]=useState('')
+   const [allDogs, setAllDogs]=useState([])
    useEffect(() =>{
      axios.get(`http://localhost:3000/dogs`)
         .then((result) =>{
@@ -27,19 +28,44 @@ export default function Dogs(props) {
     }, [])
     const params =useParams();
     const IUD =params.id
-    console.log(yourDog)
+    console.log(`Your dog logged from state: `, yourDog)
+
+    useEffect(()=>{
+        axios.get(`http://localhost:3000/dogs`)
+        .then((result) =>{
+            let allDogs = result.data
+            setAllDogs(allDogs)
+        })
+        .catch(err => console.log(err))
+    }, [])
+    console.log(`All dogs logged from state: `, allDogs, typeof allDogs)
+    
+
    //I can get the data to console, but I can't get it to return, and therefor be able to be mapped
     return (
         <>
        <div className="matchingChart">
        <div className="yourInfo">
-        {userLocalStorage_parsed.username && <>
-        <h3>Your's dogs info:</h3>
+        {yourDog.username && <>
+        <h3>{yourDog.username}'s dogs info:</h3>
+            <p>Name: {yourDog.dogName}</p>
+            <p>Age: {yourDog.age}</p>
             <p>This is where I want to list the users information</p>
             </>}
        </div>
        <div className="otherDog">
-        <h3>Other Dogs To play with:{yourDog.username}</h3>
+        <h3>Other Dogs To play with:</h3>
+        <p>This is where I want to map all the dogs</p>
+           {allDogs.map((dog, index) => (
+            <div key={index} className='eachDog'>
+                {/* I need to clean up my data before this actually shows up, if there is an instance without a field it will give an error */}
+                <p>Owner's Contact: {dog.email}</p>
+                <p>Dog: {dog.dogName}</p>
+                <p>Dog's Age :{dog.age}</p>
+                <Link to={`/playdates/createplaydate/${dog._id}`}>Make A playdate</Link>
+            </div>
+           ))}
+        
        </div>
         </div>
         </>
