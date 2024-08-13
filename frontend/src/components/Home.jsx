ee1b4c8109675362ba66b8a0be9c14b3bf66d1e1
jlounsb1 <jefflounsbury@gmail.com>
2024-08-13 18:40:34 -0400

    fiddled with home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -15,9 +15,11 @@ export default function Home (props) {
         {!userLocalStorage_parsed.username &&<h1> Welcome to Doggy Playdates</h1>}      
         
         {userLocalStorage_parsed.username && <h1>Welcome: {userLocalStorage_parsed.username}</h1>}
-        
+        <br />
         <p>This site allows you to sign up to interact with other dogs and set up play dates. To get started hit the signin/register button.</p>
+        <br />
         <p>Once you have registered go ahead and sign in, then check out the dog list page to see a list of other users and their dogs. This is where you can see a button to set up a play time. Make sure to provide all the information the other user would need to have a successful playdate.</p>
+        <br />
         <p>To see your own playdates, just go to the play dates tab. You should be able to see all of your playdates there.</p>
         
         </div>
